433d8524ca299e904a03a07df1cbbfe78b6bd6b4
Tim Simpson <TSimpson@sme.org>
2018-02-17 16:53:04 -0500

    updated

--- index.js
+++ index.js
@@ -10,9 +10,34 @@ var modal = document.getElementById('id07');
 var modal = document.getElementById('id08');
 var modal = document.getElementById('id09');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+$('.row').on('click', 'div', function(){
+  $('#id01 h1 span').text($(this).text())
+  $('#id01').show();
+});
+
+// $(document).ready(function()
+// {
+//   $("div").css("border-radius", "50%");
+// });
+
+// $('wrapper').on('click',function(evt){
+//   var id =$(evt.target).attr('id');
+//   if ($evt.target).attr('id') === 'Table 1') {
+//     console.log('');
+// }
+// });
+
+
+// var tables = [];
+// $('button').on('click',function(){
+//   var input 1 = $('#one');
+//   var input 2 = $('#two');
+//   var input 3 = $('#three')
+//   var = reserved = {
+//     name: input1.val()
+//     number: input2.val()
+//     size: input3.val()
+//   }
+//   tables.push(reserved);
+//   console.log(tables);
+// });
