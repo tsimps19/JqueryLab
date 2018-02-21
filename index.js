16ef275b4b32540ac523abb56f437b37d61d2c28
Tim Simpson <TSimpson@sme.org>
2018-02-20 21:36:08 -0500

    update

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
 // Get the modal
 var modal = document.getElementById('id01');
-var modal = document.getElementById('id02');
-var modal = document.getElementById('id03');
-var modal = document.getElementById('id04');
-var modal = document.getElementById('id04');
-var modal = document.getElementById('id05');
-var modal = document.getElementById('id06');
-var modal = document.getElementById('id07');
-var modal = document.getElementById('id08');
-var modal = document.getElementById('id09');
+var currentTable;
 
 $('.row').on('click', 'div', function(){
+  currentTable = $(this);
   $('#id01 h1 span').text($(this).text())
   $('#id01').show();
 });
 
-// $(document).ready(function()
-// {
-//   $("div").css("border-radius", "50%");
-// });
-
-// $('wrapper').on('click',function(evt){
-//   var id =$(evt.target).attr('id');
-//   if ($evt.target).attr('id') === 'Table 1') {
-//     console.log('');
-// }
-// });
+var customer_reservation = [];
+$('.savebtn').on('click', function(){
+  var input1 = $('#one');
+  var input2 = $('#two');
+  var input3 = $('#three');
 
 
-// var tables = [];
-// $('button').on('click',function(){
-//   var input 1 = $('#one');
-//   var input 2 = $('#two');
-//   var input 3 = $('#three')
-//   var = reserved = {
-//     name: input1.val()
-//     number: input2.val()
-//     size: input3.val()
-//   }
-//   tables.push(reserved);
-//   console.log(tables);
-// });
+
+  currentTable.addClass('reserved');
+  currentTable.append(`
+  <div class="hidden">
+    <p>Name: ${input1.val()}</p>
+    <p>Seats: ${input3.val()}</p>
+  </div>
+  `)
+  console.log(input1);
+
+  // customer_reservation.push(reserved);
+  $('#id01').hide();
+});
